bd1f542658b311f7fbac412bcd68586a98166a0b
Hoang Long <hoanglong0930@gmail.com>
2019-04-23 19:19:32 +0300

    simple UI

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Field from './Field';
+
+function App() {
+  return (
+    <div className="ui container">
+      <div className="ui form">
+        <Field />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Field.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div className="ui field">
+            <label>Enter text: </label>
+            <input />
+        </div>
+    );
+}
